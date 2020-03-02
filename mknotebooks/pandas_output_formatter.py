3d385ccf42bd92bf383fab26c32454d7ad2904b2
Jono Gray <jonathan.gray@flowminder.org>
2020-03-02 13:25:06 +0000

    Add html formatter for pandas tables

--- mknotebooks/pandas_output_formatter.py
+++ mknotebooks/pandas_output_formatter.py
@@ -5,5 +5,8 @@ try:
     get_ipython().display_formatter.formatters["text/markdown"].for_type(
         pd.DataFrame, lambda x: x.to_markdown()
     )
+    get_ipython().display_formatter.formatters["text/html"].for_type(
+        pd.DataFrame, lambda x: x.to_markdown()
+    )
 except ImportError:
     pass  # No pandas or no tabulate
